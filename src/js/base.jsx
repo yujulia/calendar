6c7ae55204e984669cc21bb61ecffc857f412450
jyuya <juliayu@gmail.com>
2015-06-01 23:42:23 -0700

    add jquery test for debounce

--- src/js/base.jsx
+++ src/js/base.jsx
@@ -4,11 +4,18 @@
 import React from "react";
 import Calendar from "calendar.jsx";
 import $ from "jquery";
+import _ from "underscore";
+
+function clickedH1(){
+    console.log("h1 click");
+}
 
 // ------------------------------- load our app
 
 function loadCalendar(){
     React.render(<Calendar />, document.getElementById("calendar-app"));
+
+    $("h1").click(_.debounce(clickedH1, 400));
 };
 
 // ------------------------------- on dom ready
